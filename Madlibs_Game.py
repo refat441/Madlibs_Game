06262930b89dd696d150e5fbc421770dd9db77c3
refat441 <refatahmed441@gmail.com>
2026-07-23 19:07:57 +0600

    all at once

---
+++ Madlibs_Game.py
@@ -0,0 +1,16 @@
+# Madlibs Game
+# Word game where you can creat your story
+# By filling in the blanks with random words ]
+
+noun1 = input("enter your name : ")
+adjective1 = input("enter an adjective : ")
+noun2 = input("enter noun (place thing etc) : ")
+adjective2 = input("enter an adjective (discription) : ")
+verb1 = input("inter a verb which end with 'ing' : ")
+adjective3 = input("enter an adjective : ")
+
+print(f"My name is {noun1}.")
+print(f"Today I went to {adjective1} zoo.")
+print(f"In an exhibit, I saw a {noun2}.")
+print(f"{noun2} was {adjective2} and {verb1}.")
+print(f"I was {adjective3}.")
---
+++ main.py
@@ -0,0 +1,101 @@
+# # #   string name 
+# # name = "refat"
+# # age = "nine"
+# # print(f"my name is : {name}")
+# # print (f"my age is : {age} {name}")
+# # print (f"{name}    {age}")
+
+# # # integer 
+# # date = 25
+# # book = 5
+# # print(f"today date is {date}")
+# # print(f"today book count is : {book}")
+
+# # #float 
+# # money = 10.45
+# # print(f"return ${money}")
+
+# # #boolean
+
+# # school = True
+# # travel = False
+
+# # if school:
+# #     print("i am going to school")
+# # else:
+# #     print("i am not going to school")
+
+# # if travel:
+# #     print("going....")
+# # else:
+# #     print(f"not going....{book}")
+
+# print ("refat ahmed bhuiyann")
+
+# print ("refat")
+
+# bookish = "bookish"
+# print (bookish)
+# print (f"my book number is {bookish}1")
+
+# book = "bangla"
+# print (book)
+
+# number = 10
+# print (f"my number is {number}")
+
+# curret = False;
+
+# if curret :
+#     print ("comeing....")
+
+# else  :
+#     print (f"not comening.... {number}")
+
+# one_number = 50;
+# two_number = -100;
+# sum_one = one_number - two_number;
+
+# print (f"the number is = {sum_one}")
+
+# if 5 > 6:
+#     print("five is lower")
+    
+# print ("Hello World!")
+
+# print ("Have a good day.")
+
+# print ("Learning Python is fun!")
+# print (type(number))
+
+
+# print (type(one_number))
+# change = float (one_number)
+# print (change)
+
+# name = input ("what is your name ")
+# age = int(input("what is your age "))
+
+# age += 1
+
+# print(f"my name is {name}")
+# print("happy Birthday")
+# print(f"my age is {age}")
+
+
+# length = input ("enter lenth = ")
+# width = input ("enter width = ")
+
+# area = int(length)*int(width)
+
+# print(f"the width is = {area}")
+
+item = input ("enter the item = ")
+price = input ("enter the price = ")
+quantity = input ("enter the quentity = ")
+
+total = float(price)* float(quantity)
+
+
+print(f"the item is = {quantity} * {item}")
+print(f"sub total = {total}")
